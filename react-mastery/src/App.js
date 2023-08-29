f1b607f3f966fa7f9b8fc7dea82ac766a48dcf1c
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-08-29 23:24:07 +0600

    learning about component

---
+++ react-mastery/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './Pages/Home'
+import About from './Pages/About';
+function App() {
+  return (
+   <div className='App'>
+    <Home/>
+    <About/>
+   </div>
+  );
+}
+
+export default App;
